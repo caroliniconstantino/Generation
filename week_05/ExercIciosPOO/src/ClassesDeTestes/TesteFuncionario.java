d713bc9b939eb41d764a7f0febfc0fe2e383af0e
Carol_Constantino <caroliniconstantino@gmail.com>
2023-01-16 17:15:06 -0300

    POO

--- week_05/ExercIciosPOO/src/ClassesDeTestes/TesteFuncionario.java
+++ week_05/ExercIciosPOO/src/ClassesDeTestes/TesteFuncionario.java
@@ -3,7 +3,7 @@ package ClassesDeTestes;
 import Classes.Funcionario;
 
 public class TesteFuncionario {
-    public  static  void  principal ( String [] args ) {
+    public  static  void  main ( String [] args ) {
         Funcionario funcionario1 = new  Funcionario ( 10230 , "Felipe" , "Desenvolvedor" , 6500 , "Osasco-SP" );
         Funcionario  funcionario2 = new  Funcionario ( 153430 , "Carol" , "Desenvolvedor" , 6500 , "Porto Alegre-RS" );
 
--- week_05/ExercIciosPOO/src/ClassesDeTestes/TesteProduto.java
+++ week_05/ExercIciosPOO/src/ClassesDeTestes/TesteProduto.java
@@ -3,7 +3,7 @@ package ClassesDeTestes;
 import Classes.Produto;
 
 public class TesteProduto {
-    public  static  void  principal ( String [] args ) {
+    public  static  void  main ( String [] args ) {
         Produto Produto1 = new Produto ( 1234 , "Celular" , "eletrônicos" , 5000 , 25 );
         Produto  Produto2 = new Produto ( 4532 , "Toalha" , "Casa" , 60 , 50 );
 
