ea45924492f22e3260b9c0ed1ad3974a8d9a3644
Carol_Constantino <caroliniconstantino@gmail.com>
2023-01-17 11:53:08 -0300

    exerc heranas

---
+++ week_05/heranca/src/Entrega.java
@@ -0,0 +1,133 @@
+public class Entrega {
+//    public class PessoaFisica extends Cliente {
+//        private long cpf;
+//
+//        public PessoaFisica(int id, String nome, String email, long telefone, long nascimento, long cpf) {
+//            super(id, nome, email, telefone, nascimento);
+//            this.cpf = cpf;
+//        }
+//
+//        public long getCpf() {
+//            return cpf;
+//        }
+//
+//        public void setCpf(long cpf) {
+//            this.cpf = cpf;
+//        }
+//
+//        @Override
+//        public void visualizar(){
+//            super.visualizar();
+//            System.out.println("******************************");
+//            System.out.println("O CNPJ do usuário é: " + this.cpf);
+//        }
+//    }
+
+//    public class PessoaJuridica extends Cliente {
+//        public double cnpf;
+//
+//        public PessoaJuridica(int id, String nome, String email, long telefone, long nascimento, double cnpf) {
+//            super(id, nome, email, telefone, nascimento);
+//            this.cnpf = cnpf;
+//        }
+//
+//        public double getCnpf() {
+//            return cnpf;
+//        }
+//
+//        public void setCnpf(double cnpf) {
+//            this.cnpf = cnpf;
+//        }
+//
+//        @Override
+//        public void visualizar(){
+//            super.visualizar();
+//            System.out.println("******************************");
+//            System.out.println("O CNPJ do usuário é: " + this.cnpf);
+//        }
+//    }
+
+//    public class Cliente {
+//        public int id;
+//        public String nome;
+//        public String email;
+//        public long telefone;
+//        public long nascimento;
+//
+//        public Cliente(int id, String nome, String email, long telefone, long nascimento) {
+//            this.id = id;
+//            this.nome = nome;
+//            this.email = email;
+//            this.telefone = telefone;
+//            this.nascimento = nascimento;
+//        }
+//
+//        public int getId() {
+//            return id;
+//        }
+//
+//        public void setId(int id) {
+//            this.id = id;
+//        }
+//
+//        public String getNome() {
+//            return nome;
+//        }
+//
+//        public void setNome(String nome) {
+//            this.nome = nome;
+//        }
+//
+//        public String getEmail() {
+//            return email;
+//        }
+//
+//        public void setEmail(String email) {
+//            this.email = email;
+//        }
+//
+//        public long getTelefone() {
+//            return telefone;
+//        }
+//
+//        public void setTelefone(int telefone) {
+//            this.telefone = telefone;
+//        }
+//
+//        public long getNascimento() {
+//            return nascimento;
+//        }
+//
+//        public void setNascimento(int nascimento) {
+//            this.nascimento = nascimento;
+//        }
+//
+//        public void visualizar(){
+//            System.out.println(
+//                    "O id do cliente é: " + id +
+//                            "\nO nome do cliente é: " + nome +
+//                            "\nO email do cliente é: " + email +
+//                            "\nO telefone do cliente é: " + telefone +
+//                            "\nO data de nascimento do cliente é: " + nascimento);
+//        }
+//    }
+//public class TestaCliente {
+//    public static void main(String[] args) {
+//        PessoaFisica PF01 = new PessoaFisica(984938, "Edlane", "ed@gmail.com", 893493849, 110388, 928392832);
+//        PessoaFisica PF02 = new PessoaFisica(773829, "Abner", "abner@gmail.com", 993842032, 120190, 293829382);
+//
+//        PessoaJuridica PJ01 = new PessoaJuridica(192302, "Alexia", "alexia@gmail.com", 293829382, 200594, 29302930);
+//        PessoaJuridica PJ02 = new PessoaJuridica(293239, "Anna", "anna@gmail.com", 93894839, 270997, 293029329);
+//
+//        System.out.println("------------Pessoa Física-----------");
+//        PF01.visualizar();
+//        System.out.println();
+//        PF02.visualizar();
+//        System.out.println("-----------Pessoa Juridica----------");
+//        PJ01.visualizar();
+//        System.out.println();
+//        PJ02.visualizar();
+//
+//    }
+//}
+}
