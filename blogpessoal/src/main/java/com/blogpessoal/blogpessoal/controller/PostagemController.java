472b6b9579a5f289dd041ba934ef241add49aa96
Carol_Constantino <caroliniconstantino@gmail.com>
2023-01-31 10:50:40 -0300

    enviando put concluído

--- blogpessoal/src/main/java/com/blogpessoal/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/com/blogpessoal/blogpessoal/controller/PostagemController.java
@@ -44,6 +44,14 @@ public class PostagemController {
                 .body(postagemRepository.save(postagem));
     }
 
+    @PutMapping
+    public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+        return postagemRepository.findById(postagem.getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK)
+                        .body(postagemRepository.save(postagem)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id){
