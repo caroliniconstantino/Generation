02bc13339df6dc5fe4925a2a8aced92bd6686199
Carol_Constantino <caroliniconstantino@gmail.com>
2023-02-07 16:44:32 -0300

    enviando @Repository que faltou

--- blogpessoal/src/main/java/com/blogpessoal/blogpessoal/repository/UsuarioRepository.java
+++ blogpessoal/src/main/java/com/blogpessoal/blogpessoal/repository/UsuarioRepository.java
@@ -2,9 +2,11 @@ package com.blogpessoal.blogpessoal.repository;
 
 import com.blogpessoal.blogpessoal.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
 
     public Optional<Usuario> findByUsuario(String usuario);
